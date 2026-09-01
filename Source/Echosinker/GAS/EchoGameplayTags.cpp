4c5564c46f8e4b2bf716490537cf2c47c707270c
Siguning <siguning@gmail.com>
2026-09-01 21:29:03 +0900

    feat: Gameplay Tag declaration

---
+++ Source/Echosinker/GAS/EchoGameplayTags.cpp
@@ -0,0 +1,16 @@
+﻿#include "EchoGameplayTags.h"
+
+namespace EchoGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Attack_Light, "Ability.Attack.Light");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Attack_Heavy, "Ability.Attack.Heavy");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Equipment_Secondary, "Ability.Equipment.Secondary");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Equipment_Skill, "Ability.Equipment.Skill");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Dodge, "Ability.Dodge");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Skill_Slot1, "Ability.Skill.Slot1");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Skill_Slot2, "Ability.Skill.Slot2");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Item_Heal, "Ability.Item.Heal");
+ 
+	UE_DEFINE_GAMEPLAY_TAG(State_Sprinting, "State.Sprinting");
+	UE_DEFINE_GAMEPLAY_TAG(State_LockedOn, "State.LockedOn");
+}
---
+++ Source/Echosinker/GAS/EchoGameplayTags.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+ 
+namespace EchoGameplayTags
+{
+	ECHOSINKER_API extern FNativeGameplayTag Ability_Attack_Light;
+	ECHOSINKER_API extern FNativeGameplayTag Ability_Attack_Heavy;
+	ECHOSINKER_API extern FNativeGameplayTag Ability_Equipment_Secondary;
+	ECHOSINKER_API extern FNativeGameplayTag Ability_Equipment_Skill;
+	ECHOSINKER_API extern FNativeGameplayTag Ability_Dodge;
+	ECHOSINKER_API extern FNativeGameplayTag Ability_Skill_Slot1;
+	ECHOSINKER_API extern FNativeGameplayTag Ability_Skill_Slot2;
+	ECHOSINKER_API extern FNativeGameplayTag Ability_Item_Heal;
+ 
+	ECHOSINKER_API extern FNativeGameplayTag State_Sprinting;
+	ECHOSINKER_API extern FNativeGameplayTag State_LockedOn;
+}
+ 
