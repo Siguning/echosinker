94edefa1d5484716b6c39fd7f48800572ea4db5c
Siguning <siguning@gmail.com>
2026-09-01 21:29:39 +0900

    feat: interactable interface declaration

---
+++ Source/Echosinker/System/Echointeractable.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EchoInteractable.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UEchoInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ECHOSINKER_API IEchoInteractable
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, Category = "Echo|Interaction")
+	void OnInteract(AActor* Interactor);
+};
